457a780b119a1864b7f00e192eb93cd25ef27ecb
Sergio Di Fiore <difiores@outlook.com>
2018-06-12 15:10:04 -0300

    Olá Mundo

---
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonkeyHubApp.ViewModels
+{
+    public class MainViewModel
+    {
+        public string Descricao { get; set; }
+
+        public MainViewModel()
+        {
+            Descricao = "Olá mundo! Eu estou aqui";
+        }
+    }
+}
