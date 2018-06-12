b5832386fb311df3b9cc7590c48018436caba681
Sergio Di Fiore <difiores@outlook.com>
2018-06-12 16:18:45 -0300

    BaseViewModel

--- MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MonkeyHubApp.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : BaseViewModel
     {
-        public string Descricao { get; set; }
+        private string _descricao;
+
+        public string Descricao 
+        {
+            get { return _descricao; }
+            set { SetProperty(ref _descricao, value); }
+        }
 
         public MainViewModel()
         {
-            Descricao = "Olá mundo! Eu estou aqui";
+            Descricao = "Olá mundo! Eu estou aqui!";
+
+            Task.Delay(3000).ContinueWith(async t =>
+            {
+                Descricao = "Meu texto mudou";
+
+                for (int i = 1; i < 10; i++)
+                {
+                    await Task.Delay(1000);
+                    Descricao = $"Meu texto mudou {i}";
+                }
+            });
         }
     }
 }
